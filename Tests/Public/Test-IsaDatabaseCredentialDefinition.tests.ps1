6edc42837e232cdcac11495119b1f6f536ecfc38
Wagner Wolfgang (danubie) <wolfgang.wagner@inhouse.wko.at>
2019-10-24 18:51:09 +0200

    add some tests (e.g. pipelineing)

--- Tests/Public/Test-IsaDatabaseCredentialDefinition.tests.ps1
+++ Tests/Public/Test-IsaDatabaseCredentialDefinition.tests.ps1
@@ -1,6 +1,5 @@
-# . $PSScriptRoot\..\WWSpielwiese\Public\Test-IsaDatabaseCredentialDefinition.ps1
 Remove-Module -Name IsaWebSqlToolbox -Force -ErrorAction SilentlyContinue
-Import-Module $PSScriptRoot\..\..\DanTools\DanTools.psm1
+Import-Module $PSScriptRoot\..\..\IsaWebSqlToolbox\IsaWebSqlToolbox.psm1
 
 $dbUserList = @(
     [PSCustomObject]@{  InstanceName = 'Instance1';     Database = 'db1';       Login = 'DOMAIN\User11';     AuthenticationType = 'Windows'   }
@@ -67,6 +66,57 @@ Describe 'Test-IsaDatabaseCredentialDefinition@DB: ' {
             $result = Test-IsaDatabaseCredentialDefinition -DbUser $dbUserSingle -InputObject $zuPruefen
             $result.AccountName | Should -Be 'DOMAIN\User11'
             $result.Isvalid | Should -BeTrue
+        }
+    }
+    Context "Große Liste an DBUsern" {
+        It "2 gueltig; Rest ungueltig" {
+            $zuPruefen1 = [PSCustomObject] @{ InstanceName = 'Instance1'; Database = 'db1'; AccountName = 'DOMAIN\User11' }
+            $zuPruefen2 = [PSCustomObject] @{ InstanceName = 'Instance1'; Database = 'db2'; AccountName = 'DOMAIN\User21' }
+            $zuPruefen3 = [PSCustomObject] @{ InstanceName = 'Instance1'; Database = 'db1'; AccountName = 'DOMAIN\huhu' }
+            $result = Test-IsaDatabaseCredentialDefinition -DbUser $dbUserList -InputObject ($zuPruefen1,$zuPruefen2,$zuPruefen3)
+            $result.Count | Should -Be 3
+            $valid = $result.Where({ $_.Isvalid })
+            $valid.Count        | Should -Be 2
+            $valid[0].AccountName     | Should -Be 'DOMAIN\User11'
+            $valid[0].Database  | Should -Be 'db1'
+            $valid[1].AccountName     | Should -Be 'DOMAIN\User21'
+            $valid[1].Database  | Should -Be 'db2'
+        }
+        It "User11 in 3 DBs, User12 in 1 DB" {
+            $zuPruefen1 = [PSCustomObject] @{ InstanceName = 'Instance1'; Database = '*';   AccountName = 'DOMAIN\User11' }
+            $zuPruefen2 = [PSCustomObject] @{ InstanceName = 'Instance1'; Database = 'db1'; AccountName = 'DOMAIN\User12' }
+            $zuPruefen3 = [PSCustomObject] @{ InstanceName = 'Instance1'; Database = 'db1'; AccountName = 'DOMAIN\huhu' }
+            $result = Test-IsaDatabaseCredentialDefinition -DbUser $dbUserList -InputObject ($zuPruefen1,$zuPruefen2,$zuPruefen3)
+            $result.Count | Should -Be 3
+            $valid = $result.Where({ $_.Isvalid })
+            $valid.Count        | Should -Be 2
+            $valid[0].AccountName     | Should -Be 'DOMAIN\User11'
+            $valid[0].Database  | Should -Be '*'
+            $valid[1].AccountName     | Should -Be 'DOMAIN\User12'
+            $valid[1].Database  | Should -Be 'db1'
+        }
+        It "keiner von den drei erlaubten trifft zu: 0 valid" {
+            $zuPruefen1 = [PSCustomObject] @{ InstanceName = 'Instance1'; Database = 'db1'; AccountName = 'DOMAIN\hoho' }
+            $zuPruefen2 = [PSCustomObject] @{ InstanceName = 'Instance1'; Database = '*';   AccountName = 'DOMAIN\haha' }
+            $result = Test-IsaDatabaseCredentialDefinition -DbUser $dbUserList -InputObject ($zuPruefen1,$zuPruefen2)
+            $result.Count       | Should -Be 2
+            $valid = $result.Where({ $_.Isvalid })
+            $valid              | Should -BeNullOrEmpty
+        }
+    }
+    Context "Pipeline direkt" {
+        It "3 erlaubte; aber nur 1 kommt zurück (Verena)" {
+            $zuPruefen1 = [PSCustomObject] @{ InstanceName = 'Instance1'; Database = 'db1'; AccountName = 'DOMAIN\hoho' }
+            $zuPruefen2 = [PSCustomObject] @{ InstanceName = 'Instance2'; Database = 'db3'; AccountName = 'DOMAIN\User11' }
+            $result = $dbUserList   | Test-IsaDatabaseCredentialDefinition -InputObject ($zuPruefen1,$zuPruefen2)
+            $result[0].AccountName     | Should -Be 'DOMAIN\hoho'
+            $result[0].Database        | Should -Be 'db1'
+            $result[0].InstanceName    | Should -Be 'Instance1'
+            $result[0].Isvalid          | Should -Be $false
+            $result[1].AccountName     | Should -Be 'DOMAIN\User11'
+            $result[1].Database        | Should -Be 'db3'
+            $result[1].InstanceName    | Should -Be 'Instance2'
+            $result[1].Isvalid          | Should -Be $true
         }
     }
 }
