946506bf58d986b479ad2693021161f842772517
Wagner Wolfgang (danubie) <wolfgang.wagner@inhouse.wko.at>
2019-10-24 18:32:36 +0200

    command help added

--- DanTools/Public/Test-IsaDatabaseCredential.ps1
+++ DanTools/Public/Test-IsaDatabaseCredential.ps1
@@ -1,3 +1,48 @@
+<#
+.SYNOPSIS
+Checks database users (coming from Get-DbaDBUsers) are still valid
+
+.DESCRIPTION
+Based on a definition list (Instance, DB, Account) alle Windows-Logins are checked if the still should be allowed to access a database.
+A list is returned and each inpu user object receives a flag "IsValid" to indicate that this account was found based on the conditions of the validation list.
+
+.PARAMETER DbUser
+List of user objects (like received from Get-DbaDbUser) to be validated. 
+Of course One can use it's own object type. The object has to have the following attributes (all of type string):
+InstanceName    : Name of the database-instance
+Database        : Name of the database
+Login           : Login in form of 'DOMAIN\User11'
+AuthenticationType = 'Windows'
+
+.PARAMETER InstanceName
+Instance to which a Login is allowed to connect
+
+.PARAMETER Database
+Database to which a Login is allowed to connect
+
+.PARAMETER AccountName
+Windows Account allowed to connect
+
+.PARAMETER InputObject
+List of hashtable containing the definition of the valid instance/db/userlogin pairs
+
+.EXAMPLE
+$userList = Get-DbaDbUser -SqlInstance 'localhost:55555' -Database db1; $userChecked = Test-IsaDatabaseCredential -DBUser $userList -InstanceName 'Instance1' -Database 'db1' -AccountName 'DOMAIN\User11'
+$valid = $userChecked.Where({ $_.IsValid})
+Checks for the presence of just one user definition
+
+.EXAMPLE
+$userList | Test-IsaDatabaseCredential -InputObject (Get-Content definition.csv | Convert-FromCsv )
+Checks a user list (e.g. from Get-DbaDbUser) against a validation list defined in a csv
+The CSV must have the following form
+InstanceName;Database;AccountName
+Instance1;db1;DOMAIN\User1
+Instance1;db1;DOMAIN\User2
+Instance2;db1;DOMAIN\User3
+
+.NOTES
+General notes
+#>
 function Test-IsaDatabaseCredential {
     [cmdletbinding()]
     param (
