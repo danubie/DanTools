ba5b5595818ad2c405ab3f9507f506d92f1f652c
Wolfgang Wagner <wolfgang.wagner@inhouse.wko.at>
2021-03-24 09:39:07 +0100

    Write-Warnings -> Verbose

--- DanTools/DanTools.psm1
+++ DanTools/DanTools.psm1
@@ -1,13 +1,13 @@
-Write-Warning "psm1: $PSScriptRoot"
+Write-Verbose "psm1 Script-Root: $PSScriptRoot"
 $private = Get-ChildItem (Join-Path $PSScriptRoot "Private") -Recurse -Filter '*.ps1'
 $public = Get-ChildItem (Join-Path $PSScriptRoot "Public") -Recurse -Filter '*.ps1'
 foreach ($function in $private) {
     . $function.FullName
-    Write-Warning "private: $($function.FullName)"
+    Write-Verbose "Include private: $($function.FullName)"
 }
 foreach ($function in $public) {
     . $function.FullName
-    Write-Warning "public: $($function.FullName)"
+    Write-Verbose "Include public: $($function.FullName)"
 }
 
 Export-ModuleMember -Function $public.BaseName -Verbose
